8df607edef8ce4ca069666c870f22c30333df61a
Luiz Eduardo Cajueiro Crisanto <luiz.crisanto@gmail.com>
2022-09-29 21:18:25 +0000

    100% DONE

--- compiladores-main/AnaliseLexica/CompiladorL3.java
+++ compiladores-main/AnaliseLexica/CompiladorL3.java
@@ -6,7 +6,7 @@ public class CompiladorL3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Lexico lexico = new Lexico("/workspace/compiladores/AnaliseLexica/codigo.txt");
+        Lexico lexico = new Lexico("/workspace/compilador/compiladores-main/AnaliseLexica/codigo.txt");
         Token t = null;
         while((t = lexico.nextToken()) != null){
             System.out.println(t.toString());
